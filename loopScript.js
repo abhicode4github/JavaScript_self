358c945a7234f215c3232be4213378715118c9ba
Abhishek jha <abhishekjhagzb2001@gmail.com>
2023-07-17 00:22:31 +0530

    Update loopScript.js

--- loopScript.js
+++ loopScript.js
@@ -1,3 +1,17 @@
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weights repetition ${rep}`);
 }
+
+
+// OUTPUT 
+
+Lifting weights repetition 1
+loopScript.js:2 Lifting weights repetition 2
+loopScript.js:2 Lifting weights repetition 3
+loopScript.js:2 Lifting weights repetition 4
+loopScript.js:2 Lifting weights repetition 5
+loopScript.js:2 Lifting weights repetition 6
+loopScript.js:2 Lifting weights repetition 7
+loopScript.js:2 Lifting weights repetition 8
+loopScript.js:2 Lifting weights repetition 9
+loopScript.js:2 Lifting weights repetition 10
