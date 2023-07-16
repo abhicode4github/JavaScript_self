323c4706e11a77cfc8dc028db75ba88b44133770
Abhishek jha <abhishekjhagzb2001@gmail.com>
2023-07-17 00:28:01 +0530

    Add files via upload

---
+++ backLoopScript.js
@@ -0,0 +1,12 @@
+const jonas = [
+  "Jonas",
+  "Sharma",
+  2037 - 2001,
+  "student",
+  ["jonny", "Karlos", "dark"],
+  true,
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
