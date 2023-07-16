d22fcddbc79010da6a42a6cc39b438c525332d11
Abhishek jha <abhishekjhagzb2001@gmail.com>
2023-07-17 00:06:34 +0530

    Update objectScript.js

--- objectScript.js
+++ objectScript.js
@@ -5,3 +5,42 @@ const jonas = {
   job: "student",
   friends: ["jonny", "Karlos", "dark"],
 };
+
+
+// OUTPUT 
+
+jonas;
+
+age
+: 
+36
+firstName
+: 
+"Jonas"
+friends
+: 
+Array(3)
+0
+: 
+"jonny"
+1
+: 
+"Karlos"
+2
+: 
+"dark"
+length
+: 
+3
+[[Prototype]]
+: 
+Array(0)
+job
+: 
+"student"
+lastName
+: 
+"Sharma"
+[[Prototype]]
+: 
+Object
